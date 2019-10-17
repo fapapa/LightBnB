727e48fef017fa47b45f0dacfe2a7c670abcf592
Fabio Papa <fabtheman@gmail.com>
2019-10-17 10:40:07 -0700

    Update the get all reservations function to retrieve from db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -72,7 +72,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT reservations.*, properties.*, AVG(rating)
+       FROM reservations
+       JOIN properties
+            ON reservations.property_id = properties.id
+       JOIN property_reviews
+            ON properties.id = property_reviews.property_id
+       WHERE end_date < now()::date AND reservations.guest_id = $1
+       GROUP BY properties.id, reservations.id
+       ORDER BY reservations.start_date DESC
+       LIMIT $2;
+  `, [guest_id, limit])
+    .then(res => res.rows);
 };
 exports.getAllReservations = getAllReservations;
 
