6a461f3337ca2615cdb8356c8bcb7563e11a8d0c
Fabio Papa <fabtheman@gmail.com>
2019-10-16 20:14:21 -0700

    Implement the get all properties from the database functionality

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -76,11 +76,12 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT *
+  FROM properties
+  LIMIT $1;
+  `, [limit])
+    .then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
